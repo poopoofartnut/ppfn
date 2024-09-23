bfe84cbabca6540a3a0fcdf13816c4595f45212a
poopoofartnut <miro.kokki300@gmail.com>
2024-09-23 18:05:19 +0000

    skib

--- main/script.js
+++ main/script.js
@@ -17,20 +17,24 @@ if (!localStorage.getItem('theme')) {
        root.style.setProperty("--back", "rgb(230, 230, 230)");
        root.style.setProperty("--font", "black");
        root.style.setProperty("--tile", "rgb(200, 200, 200)");
-       document.getElementById("res").innerHTML= "<span class='material-symbols-outlined'>dark_mode</span>";
+       document.getElementById("res").innerHTML= "<span id='g' class='material-symbols-outlined'>dark_mode</span>";
        root.style.setProperty("--res", "rgb(56, 56, 56)");
        root.style.setProperty("--themeicon", "white");
    }else if (sigma == "dark"){
        root.style.setProperty("--back", "rgb(56, 56, 56)");
        root.style.setProperty("--font", "white");
        root.style.setProperty("--tile", "rgb(70, 70, 70)");
-       document.getElementById("res").innerHTML= "<span class='material-symbols-outlined'>light_mode</span>";
+       document.getElementById("res").innerHTML= "<span id='g' class='material-symbols-outlined'>light_mode</span>";
        root.style.setProperty("--res", "rgb(230, 230, 230)");
        root.style.setProperty("--themeicon", "black");
    }else{
        console.log("Invalid theme var input")
    }
  }
- var localvar;
- if(localStorage.getItem('theme')=="1"){var localvar = "dark"}else{var localvar = "light"}
- themeToggle(localvar);
+ var localskib;
+ if(localStorage.getItem('theme')=="1"){var localskib = "dark"}else{var localskib = "light"}
+ themeToggle(localskib);
+ function runSearch(query){
+  var query;
+  
+ }
